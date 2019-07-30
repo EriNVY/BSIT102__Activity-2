ac188edf49dbdaf0182b39454e86492f8ae61bc9
EriNVY <52990846+EriNVY@users.noreply.github.com>
2019-07-30 10:26:11 +0800

    Add files via upload

---
+++ Activity-2/src/Short_Intro.java
@@ -0,0 +1,26 @@
+/**
+ * @(#)Short_Intro.java
+ *
+ * Short_Intro application
+ *
+ * @author B. Ramos
+ * @version 1.00 2019/7/30
+ */
+ 
+public class Short_Intro {
+    
+    public static void main(String[] args) {
+    	
+    	String name, course, section, address, contact;
+    	
+    	name = " [REDACTED] ";
+    	course = " Bachelor of Sc**** #$#@^&&^$chnology ";
+    	section = " section 102 ";
+    	address = " [REDACTED] ";
+    	contact = " (+63)9 ********";
+    	
+    	
+    	System.out.println("Hi! my name is " + name + ". I'm a college student currently taking a " + course + ". I manage to be in class" +  section);
+    	System.out.println(" I live in " + address + ". And my contact number, if anyone is asking is " + contact + ". It's nice to meet you! ^-^");
+    }
+}
